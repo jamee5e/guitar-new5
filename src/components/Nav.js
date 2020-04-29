00aa7721900f9dc675e7dd9ab142311cd2a77b2c
Jame5e <natthawut31250@gmail.com>
2020-04-29 19:18:06 +0700

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+import { Link, withRouter } from "react-router-dom";
+
+const Nav = (props) => {
+
+
+    return(
+        <nav>
+        <ul>
+          <li><Link to="/"> ReactReduxFirebaseAuth </Link></li>
+         
+        </ul>
+        <ul>
+        <li><Link to="/create">new post</Link></li>
+       
+     
+        </ul>
+      </nav>
+    )
+
+}
+
+export default withRouter(Nav);
---
+++ src/reducers/reducers.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+import createUser from "./signin";
+
+const reducers= combineReducers({
+    signIn: createUser
+});
+
+export default reducers;
